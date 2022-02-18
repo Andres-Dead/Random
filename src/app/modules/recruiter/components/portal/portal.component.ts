fc2354be4de231b731538af8b2ac82d2dd34f839
Carlos Cendejas <carloscb02@outlook.com>
2022-02-17 21:19:15 -0600

    fix merge

--- src/app/modules/recruiter/components/portal/portal.component.ts
+++ src/app/modules/recruiter/components/portal/portal.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { RecruiterServiceService } from '../../services/recruiter-service.service';
 
 @Component({
   selector: 'app-portal',
   templateUrl: './portal.component.html',
-  styleUrls: ['./portal.component.css']
+  styleUrls: ['./portal.component.css'],
 })
 export class PortalComponent implements OnInit {
+  token: string = localStorage.getItem('token');
 
-  constructor() { }
+  result: any;
+
+  constructor(private readonly recrutierService: RecruiterServiceService) {}
 
   ngOnInit(): void {
+    this.getDataPortal();
   }
 
+  // get data to portal
+  getDataPortal() {
+    this.recrutierService.getWorkTableVacants(this.token).subscribe(
+      (data) => {
+        this.result = data;
+        console.log(data);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/modules/recruiter/services/recruiter-service.service.ts
+++ src/app/modules/recruiter/services/recruiter-service.service.ts
@@ -6,103 +6,133 @@ import Swal from 'sweetalert2';
 import { of } from 'rxjs/internal/observable/of';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecruiterServiceService {
-
   constructor(
-    private httpService : HttpClient,
-    private session     : SessionService
-  ) { }
+    private httpService: HttpClient,
+    private session: SessionService
+  ) {}
 
-  public getWorkTableVacants(token){
-    return this.httpService.get(`${this.session.API}recruiterTable/getActiveVacants`, {
-      headers: new HttpHeaders({ Authorization: token }),
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  // service GET portal component
+  public getWorkTableVacants(token) {
+    return this.httpService
+      .get(`${this.session.API}recruiterTable/getActiveVacants`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
 
-  public getWorkTableInactiveVacants(token){
-    return this.httpService.get(`${this.session.API}recruiterTable/getInactiveVacants`, {
-      headers: new HttpHeaders({ Authorization: token }),
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  public getWorkTableInactiveVacants(token) {
+    return this.httpService
+      .get(`${this.session.API}recruiterTable/getInactiveVacants`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
 
-  public getLastTalent(token){
-    return this.httpService.get(`${this.session.API}recruiterTable/getLastTalent`, {
-      headers: new HttpHeaders({ Authorization: token }),
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  public getLastTalent(token) {
+    return this.httpService
+      .get(`${this.session.API}recruiterTable/getLastTalent`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
 
-  public getResources(token){
-    return this.httpService.get(`${this.session.API}recruiterTable/getResources`, {
-      headers: new HttpHeaders({ Authorization: token }),
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  public getResources(token) {
+    return this.httpService
+      .get(`${this.session.API}recruiterTable/getResources`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
 
-  public getCompany(token){
-    return this.httpService.get(`${this.session.API}recruiterTable/getCompany`, {
-      headers: new HttpHeaders({ Authorization: token})
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  public getCompany(token) {
+    return this.httpService
+      .get(`${this.session.API}recruiterTable/getCompany`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
 
-  getInfo(token){
-    return this.httpService.get(`${this.session.API}getInfo`, {
-      headers: new HttpHeaders({ Authorization: token})
-    }).pipe(timeout(30000), catchError(e=>{
-      Swal.close();
-      Swal.fire({
-        icon: 'error',
-        title: '404',
-        text: e.error.message?e.error.message:this.session.excededTime,
-        confirmButtonColor: '#1c4a83',
+  // get info profile reclutier
+  getInfo(token) {
+    return this.httpService
+      .get(`${this.session.API}getInfo`, {
+        headers: new HttpHeaders({ Authorization: token }),
       })
-      return of(null);
-    }));
+      .pipe(
+        timeout(30000),
+        catchError((e) => {
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: '404',
+            text: e.error.message ? e.error.message : this.session.excededTime,
+            confirmButtonColor: '#1c4a83',
+          });
+          return of(null);
+        })
+      );
   }
-
 }
